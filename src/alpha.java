b3185fbc4d57853ba9cecb44631abd0f5e9b2104
Jason Holdsworth <jason.holdsworth@jcu.edu.au>
2021-10-07 16:50:17 +1000

    Fix total calculations and tested with small.csv and CopperlodeTEST.csv

--- src/alpha.java
+++ src/alpha.java
@@ -18,40 +18,55 @@ public class alpha {
         {
             try {
                 double totalRainfall = 0;
-                int currentMonth = 1;
-                int count = 0;
+                int currentMonth = -1;
+                int currentYear = -1;
+
                 Reader in = new FileReader("src/CopperlodeTEST.csv");
                 Iterable<CSVRecord> records = CSVFormat.DEFAULT
                         .withHeader("code", "station", "year", "month", "day", "rainfall", "period", "quality")
                         .withSkipHeaderRecord().parse(in);
+
                 for (CSVRecord record : records) {
                     String yearRecord = record.get("year");
                     String monthRecord = record.get("month");
                     String dayRecord = record.get("day");
                     String rainfallAmount = record.get("rainfall");
                     double rainfall;
+
                     if (rainfallAmount.length() == 0) {
                         rainfall = 0;
                     } else {
                         rainfall = Double.parseDouble(rainfallAmount);
                     }
-                    int day = parseInt(dayRecord);
+
                     int year = parseInt(yearRecord);
                     int month = parseInt(monthRecord);
-                    totalRainfall += rainfall;
+
+                    if (currentMonth == -1) { // get the correct start month
+                        currentMonth = month;
+                    }
 
                     if (currentMonth != month){
                         System.out.println("Current Month" + " "+ currentMonth);
                         System.out.println("Total Rain" + " "+ totalRainfall);
                         System.out.println("Current Year" + " " + year + "\n");
                         currentMonth = month;
-                        totalRainfall = 0;
-                    }
+                        totalRainfall = rainfall;
+                        currentYear = year; // track the current year
 
+                    } else {
+                        totalRainfall += rainfall;
+                    }
 
 //                    System.out.println(totalRainfall);
 //                    System.out.println(count);
                 }
+
+                // handle any left overs
+                System.out.println("Current Month" + " "+ currentMonth);
+                System.out.println("Total Rain" + " "+ totalRainfall);
+                System.out.println("Current Year" + " " + currentYear + "\n");
+
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
